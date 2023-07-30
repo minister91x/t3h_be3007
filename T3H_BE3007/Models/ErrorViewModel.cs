bd6116b00afc73ffa10e95dcc9f6461419c07b34
quan91 <quan91.nt@gmail.com>
2023-07-30 19:40:09 +0700

    first commit

---
+++ T3H_BE3007/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T3H_BE3007.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
