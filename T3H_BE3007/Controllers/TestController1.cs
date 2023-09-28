5833d916b63d5675b73721bc5c364cd0fe8f48e6
quan91 <quan91.nt@gmail.com>
2023-09-28 21:49:11 +0700

    buoi17

---
+++ T3H_BE3007/Controllers/TestController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace T3H_BE3007.Controllers
+{
+    public class TestController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApiCore/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpPost("GetList")]
+        public async Task<ActionResult> GetList()
+        {
+            var lsst = new List<string>();
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    lsst.Add("item:" + i);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return Ok(lsst);
+        }
+    }
+}
